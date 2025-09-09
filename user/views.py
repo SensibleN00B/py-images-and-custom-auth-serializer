9c9d374503d2c50f16ed072fa9d13db3c0e6c3ab
SensibleN00B <sensible.noob@gmail.com>
2025-09-09 21:57:04 +0300

    chore: flake8 code style refactoring

--- user/views.py
+++ user/views.py
@@ -17,7 +17,9 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
     def post(self, request):
-        serializer = AuthTokenSerializer(data=request.data, context={"request": request})
+        serializer = AuthTokenSerializer(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         token, _ = Token.objects.get_or_create(user=user)
